94b83d8c50ebfce6d1b77b549cc8ca9044d73d23
givemepass <givemepassxd999@gmail.com>
2015-05-10 23:22:51 +0800

    新增組合類別

--- app/src/main/java/com/wanttospeak/cache/MyCombination.java
+++ app/src/main/java/com/wanttospeak/cache/MyCombination.java
@@ -7,24 +7,69 @@ import java.util.HashMap;
  * Created by givemepass on 2015/5/10.
  */
 public class MyCombination {
+    //二選一的陣列
+    private HashMap<String, ArrayList<MulitipleChoice>> twoOptionCombinationList;
 
-    private ArrayList<MulitipleChoice> itemsCombination;
+    //三選一的陣列
+    private HashMap<String, ArrayList<MulitipleChoice>> threeOptionCombinationList;
 
-    private HashMap<String, ArrayList<MulitipleChoice>> personalItemsCombinationList;
+    //四選一的陣列
+    private HashMap<String, ArrayList<MulitipleChoice>> fourOptionCombinationList;
 
-    public ArrayList<MulitipleChoice> getItemsCombination() {
-        return itemsCombination;
+    private static MyCombination mInstance;
+
+    private MyCombination(){}
+
+    public static MyCombination getInstance(){
+        if(mInstance == null){
+            mInstance = new MyCombination();
+        }
+        return mInstance;
     }
 
-    public void setItemsCombination(ArrayList<MulitipleChoice> itemsCombination) {
-        this.itemsCombination = itemsCombination;
+    //取得某人的組合陣列
+    public static ArrayList<MulitipleChoice> getItemsCombinationList(String personId, int type) {
+        if(type == Constant.TWO_OPTIONS) {
+            return getInstance().twoOptionCombinationList.get(personId);
+        }
+        else if(type == Constant.THREE_OPTIONS) {
+            return getInstance().twoOptionCombinationList.get(personId);
+        }
+        else if(type == Constant.FOUR_OPTIONS){
+                return getInstance().twoOptionCombinationList.get(personId);
+        } else{
+            return null;
+        }
+
     }
 
-    public HashMap<String, ArrayList<MulitipleChoice>> getPersonalItemsCombinationList() {
-        return personalItemsCombinationList;
+    //取得某人的某個組合
+    public static MulitipleChoice getItemsCombination(String personId, int index, int type) {
+        ArrayList<MulitipleChoice> choiceList = getItemsCombinationList(personId, type);
+        if(choiceList != null) {
+            return choiceList.get(index);
+        } else{
+            return null;
+        }
     }
 
-    public void setPersonalItemsCombinationList(HashMap<String, ArrayList<MulitipleChoice>> personalItemsCombinationList) {
-        this.personalItemsCombinationList = personalItemsCombinationList;
+    //設定某人的組合
+    public static void setItemCombination(String personId, MulitipleChoice choice, int type){
+        ArrayList<MulitipleChoice> mulitipleChoiceList = getInstance().getItemsCombinationList(personId, type);
+        if(mulitipleChoiceList == null){
+            mulitipleChoiceList = new ArrayList<MulitipleChoice>();
+        }
+        mulitipleChoiceList.add(choice);
+
+        if(type == Constant.TWO_OPTIONS) {
+            getInstance().twoOptionCombinationList.put(personId, mulitipleChoiceList);
+        }
+        else if(type == Constant.THREE_OPTIONS) {
+            getInstance().threeOptionCombinationList.put(personId, mulitipleChoiceList);
+        }
+        else if(type == Constant.FOUR_OPTIONS){
+            getInstance().fourOptionCombinationList.put(personId, mulitipleChoiceList);
+        }
+
     }
 }
--- app/src/main/java/com/wanttospeak/items/ItemDetailListDialog.java
+++ app/src/main/java/com/wanttospeak/items/ItemDetailListDialog.java
@@ -3,9 +3,14 @@ package com.wanttospeak.items;
 import android.content.Context;
 import android.graphics.Color;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 import com.example.givemepass.wanttospeak.R;
 import com.wanttospeak.cache.Constant;
+import com.wanttospeak.cache.DataHelper;
+import com.wanttospeak.cache.MyCombination;
 import com.wanttospeak.dialog.CommonDialog;
 
 /**
@@ -15,22 +20,60 @@ public class ItemDetailListDialog extends CommonDialog{
 
     private Context mContext;
 
+    private ListView mListView;
+
+    private ItemDetailListAdapter mAdapter;
+
+    private int type;
+
     public ItemDetailListDialog(Context context, int type) {
         super(context);
         mContext = context;
+        this.type = type;
+        initView(type);
+
+    }
+
+    private void initView(int type){
+        setContextView(R.layout.item_detail_list);
         setNaviBackgroundColor(Color.parseColor("#FFBBDD"));
         setNaviAddVisible(View.VISIBLE);
         switch(type){
             case Constant.TWO_OPTIONS:
-                setNaviBackText(context.getString(R.string.two_option));
+                setNaviBackText(mContext.getString(R.string.two_option));
                 break;
             case Constant.THREE_OPTIONS:
-                setNaviBackText(context.getString(R.string.three_option));
+                setNaviBackText(mContext.getString(R.string.three_option));
                 break;
             case Constant.FOUR_OPTIONS:
-                setNaviBackText(context.getString(R.string.four_option));
+                setNaviBackText(mContext.getString(R.string.four_option));
                 break;
         }
 
+        mListView = (ListView) findViewById(R.id.item_detail_list);
+        mAdapter = new ItemDetailListAdapter();
+        mListView.setAdapter(mAdapter);
+    }
+
+    private class ItemDetailListAdapter extends BaseAdapter{
+        @Override
+        public int getCount() {
+            return MyCombination.getItemsCombinationList(DataHelper.getCurrentPersonId(), type).size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            return null;
+        }
     }
 }
